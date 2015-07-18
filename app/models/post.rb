eb73869db2ca272295230a49b9fcd3e649ee83f8
James Ferguson <jimmy2289@gmail.com>
2015-07-18 12:17:44 +0900

    Tidied up caption validation and timeago in words.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   has_attached_file :image, styles: { :medium => "640x" }
   validates :image, presence: true
   validates :user_id, presence: true 
+  validates :caption, presence: true, length: { minimum: 3, maximum: 300 }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
